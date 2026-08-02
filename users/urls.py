c7929d86581400a93fe143a7363b99116a4a5262
altsisyphus <altsisyphus@gmail.com>
2026-08-02 23:12:34 +0300

    Добавлены маршруты приложения пользователей

---
+++ users/urls.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.views import LogoutView
+from django.urls import path, reverse_lazy
+
+from users.apps import UsersConfig
+from users.views import ProfileUpdateView, RegisterView, UserLoginView
+
+app_name = UsersConfig.name
+
+urlpatterns = [
+    path("register/", RegisterView.as_view(), name="register"),
+    path("login/", UserLoginView.as_view(), name="login"),
+    path(
+        "logout/",
+        LogoutView.as_view(next_page=reverse_lazy("catalog:home")),
+        name="logout",
+    ),
+    path("profile/", ProfileUpdateView.as_view(), name="profile"),
+]
