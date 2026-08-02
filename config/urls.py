0ebadf9d7e91f49dc2fc5190ca9826c576a9d58a
altsisyphus <altsisyphus@gmail.com>
2026-08-02 23:12:34 +0300

    Подключены маршруты пользователей

--- config/urls.py
+++ config/urls.py
@@ -8,6 +8,7 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("blogs/", include("blog.urls")),
+    path("users/", include("users.urls")),
     path("", include("catalog.urls")),
 ]
 
