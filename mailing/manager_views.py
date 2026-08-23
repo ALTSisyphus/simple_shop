29b90af46bee466538cae165ad238ec6acf30eff
altsisyphus <altsisyphus@gmail.com>
2026-08-23 23:43:32 +0300

    Добавил представления менеджера для управления рассылками

---
+++ mailing/manager_views.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views.generic import ListView
+
+from users.models import User
+from mailing.models import Mailing, Recipient
+from users.views import ManagerRequiredMixin
+
+
+class ManagerMailingListView(ManagerRequiredMixin, ListView):
+    model = Mailing
+    template_name = "mailing/manager_mailing_list.html"
+
+
+class ManagerRecipientListView(ManagerRequiredMixin, ListView):
+    model = Recipient
+    template_name = "mailing/manager_recipient_list.html"
+
+
+class ManagerUserListView(ManagerRequiredMixin, ListView):
+    model = User
+    template_name = "mailing/manager_user_list.html"
+
+
+def block_user(request, pk):
+    get_object_or_404(User, pk=pk).update(is_blocked=True)
+    return redirect("mailing:manager_users")
+
+
+def disable_mailing(request, pk):
+    mailing = get_object_or_404(Mailing, pk=pk)
+    mailing.is_active = False
+    mailing.save(update_fields=["is_active"])
+    return redirect("mailing:manager_mailings")
