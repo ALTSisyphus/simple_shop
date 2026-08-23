9a43330f690216764df732acdd4501783a1b466b
altsisyphus <altsisyphus@gmail.com>
2026-08-23 23:55:10 +0300

    Исправил контекст менеджерских представлений

--- mailing/manager_views.py
+++ mailing/manager_views.py
@@ -10,16 +10,19 @@ from users.views import ManagerRequiredMixin
 class ManagerMailingListView(ManagerRequiredMixin, ListView):
     model = Mailing
     template_name = "mailing/manager_mailing_list.html"
+    context_object_name = "mailings"
 
 
 class ManagerRecipientListView(ManagerRequiredMixin, ListView):
     model = Recipient
     template_name = "mailing/manager_recipient_list.html"
+    context_object_name = "recipients"
 
 
 class ManagerUserListView(ManagerRequiredMixin, ListView):
     model = User
     template_name = "mailing/manager_user_list.html"
+    context_object_name = "users"
 
 
 def block_user(request, pk):
