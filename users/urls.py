6831015d94d48b36992a7f3352fcf327d294dc51
altsisyphus <altsisyphus@gmail.com>
2026-08-23 23:31:24 +0300

    Добавил маршрут подтверждения email

--- users/urls.py
+++ users/urls.py
@@ -2,11 +2,17 @@ from django.contrib.auth.views import LogoutView
 from django.urls import path, reverse_lazy
 
 from users.apps import UsersConfig
-from users.views import ProfileUpdateView, RegisterView, UserLoginView
+from users.views import verify_email, ProfileUpdateView, RegisterView, UserLoginView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
+    path(
+        "verify/<str:token>/",
+        verify_email,
+        name="verify_email",
+    ),
+
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", UserLoginView.as_view(), name="login"),
     path(
